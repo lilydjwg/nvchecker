8c44cbfb0205e724494be8475dae4153fb1340aa
amazingfate <liujianfeng1994@gmail.com>
2015-09-29 12:03:15 +0800

    add github tags support

---
+++ nvchecker/source/github_tags.py
@@ -0,0 +1,22 @@
+import os
+import json
+from functools import partial
+
+from tornado.httpclient import AsyncHTTPClient
+
+GITHUB_TAGS_URL = 'https://api.github.com/repos/%s/tags'
+
+def get_version(name, conf, callback):
+    repo = conf.get('github_tags')
+    url = GITHUB_TAGS_URL % (repo)
+    headers = {'Accept': "application/vnd.github.quicksilver-preview+json"}
+    if 'NVCHECKER_GITHUB_TOKEN' in os.environ:
+        headers['Authorization'] = 'token %s' % os.environ['NVCHECKER_GITHUB_TOKEN']
+        request = HTTPRequest(url, headers=headers, user_agent='lilydjwg/nvchecker')
+        AsyncHTTPClient().fetch(request,
+                callback=partial(_github_tags_done, name, callback))
+
+def _github_tags_done(name, callback, res):
+    data = json.loads(res.body.decode('utf-8'))
+    version = data[0]['name'].strip('v')
+    callback(name, version)
