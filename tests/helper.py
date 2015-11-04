a76e283918814a1c611a1e882de2eda4b32536b8
Felix Yan <felixonmars@archlinux.org>
2015-11-04 20:50:22 +0800

    Clean up sync_get_version

--- tests/helper.py
+++ tests/helper.py
@@ -4,13 +4,8 @@ import tornado.testing
 
 class ExternalVersionTestCase(tornado.testing.AsyncTestCase):
     def sync_get_version(self, name, config):
-        result = {}
-
         def get_version_callback(name, version):
-            result["version"] = version
-            self.stop()
+            self.stop(version)
 
         get_version(name, config, get_version_callback)
-        self.wait()
-
-        return result["version"]
+        return self.wait()
