fbbc7cb1e4c89c57344ba06e7db0dc907d96e3fb
lilydjwg <lilydjwg@gmail.com>
2015-11-11 14:28:25 +0800

    bump version for PyPI
    also adjust test_gitcafe retry times

--- tests/test_gitcafe.py
+++ tests/test_gitcafe.py
@@ -3,6 +3,6 @@ from tests.helper import ExternalVersionTestCase
 
 
 class GitCafeTest(ExternalVersionTestCase):
-    @flaky(max_runs=3)
+    @flaky(max_runs=7)
     def test_gitcafe(self):
         self.assertEqual(self.sync_get_version("example", {"gitcafe": "test/test"}), "20120201")
