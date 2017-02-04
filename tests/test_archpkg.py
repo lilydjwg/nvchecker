82ca0cff9cb2e53f21a983ae437fdf6d64de5a48
lilydjwg <lilydjwg@gmail.com>
2017-02-04 16:29:59 +0800

    skip archpkg test on Travis-CI

--- tests/test_archpkg.py
+++ tests/test_archpkg.py
@@ -1,6 +1,11 @@
+import os
+
+import pytest
+
 from tests.helper import ExternalVersionTestCase
 
-
+@pytest.mark.skipif("TRAVIS" in os.environ,
+                    reason="Travis-CI has issues connecting to the Arch website")
 class ArchPKGTest(ExternalVersionTestCase):
     def test_archpkg(self):
         self.assertEqual(self.sync_get_version("ipw2100-fw", {"archpkg": None}), "1.3-7")
