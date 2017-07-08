85cad99662b9b40f9fc3e114edbbc41e798969e9
lilydjwg <lilydjwg@gmail.com>
2017-07-08 14:55:34 +0800

    tests for both tornado and aiohttp

--- nvchecker/source/__init__.py
+++ nvchecker/source/__init__.py
@@ -3,10 +3,17 @@
 
 try:
   import tornado, pycurl
+  # connection reuse, http/2
   which = 'tornado'
 except ImportError:
-  import aiohttp
-  which = 'aiohttp'
+  try:
+    import aiohttp
+    which = 'aiohttp'
+    # connection reuse
+  except ImportError:
+    import tornado
+    which = 'tornado'
+    # fallback
 
 m = __import__('%s_httpclient' % which, globals(), locals(), level=1)
 __all__ = m.__all__
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,25 +1,35 @@
 # MIT licensed
 # Copyright (c) 2017 Felix Yan <felixonmars@archlinux.org>, et al.
 
-import aiohttp
+import sys
+
+try:
+  import aiohttp
+except ImportError:
+  aiohttp = None
+
 import pytest
-pytestmark = pytest.mark.asyncio
+pytestmark = [
+  pytest.mark.asyncio,
+  pytest.mark.skipif(not aiohttp, reason="requires aiohttp"),
+]
 
 async def test_proxy(get_version, monkeypatch):
-    from nvchecker.source import session
+  sys.modules['nvchecker.source'] = sys.modules['nvchecker.source.aiohttp_httpclient']
+  from nvchecker.source import session
 
-    async def fake_request(*args, proxy, **kwargs):
-        class fake_response():
-            async def read():
-                return proxy.encode("ascii")
+  async def fake_request(*args, proxy, **kwargs):
+    class fake_response():
+      async def read():
+        return proxy.encode("ascii")
 
-            def release():
-                pass
+      def release():
+        pass
 
-        return fake_response
+    return fake_response
 
-    monkeypatch.setattr(session, "nv_config", {"proxy": "255.255.255.255:65535"}, raising=False)
-    monkeypatch.setattr(aiohttp.ClientSession, "_request", fake_request)
+  monkeypatch.setattr(session, "nv_config", {"proxy": "255.255.255.255:65535"}, raising=False)
+  monkeypatch.setattr(aiohttp.ClientSession, "_request", fake_request)
 
-    assert await get_version("example", {"regex": "(.+)", "url": "deadbeef"}) == "255.255.255.255:65535"
-    assert await get_version("example", {"regex": "(.+)", "url": "deadbeef", "proxy": "0.0.0.0:0"}) == "0.0.0.0:0"
+  assert await get_version("example", {"regex": "(.+)", "url": "deadbeef"}) == "255.255.255.255:65535"
+  assert await get_version("example", {"regex": "(.+)", "url": "deadbeef", "proxy": "0.0.0.0:0"}) == "0.0.0.0:0"
