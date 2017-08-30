f3653c79c8caa75e61f04189b64e67b20ee7fac9
lilydjwg <lilydjwg@gmail.com>
2017-08-30 17:53:25 +0800

    update tests (3rd time)

--- tests/test_debianpkg.py
+++ tests/test_debianpkg.py
@@ -5,10 +5,10 @@ import pytest
 pytestmark = pytest.mark.asyncio
 
 async def test_debianpkg(get_version):
-    assert await get_version("sigrok-firmware-fx2lafw", {"debianpkg": None}) == "0.1.3-1"
+    assert await get_version("sigrok-firmware-fx2lafw", {"debianpkg": None}) == "0.1.5-1"
 
 async def test_debianpkg_strip_release(get_version):
-    assert await get_version("sigrok-firmware-fx2lafw", {"debianpkg": None, "strip-release": 1}) == "0.1.3"
+    assert await get_version("sigrok-firmware-fx2lafw", {"debianpkg": None, "strip-release": 1}) == "0.1.5"
 
 async def test_debianpkg_suite(get_version):
     assert await get_version("sigrok-firmware-fx2lafw", {"debianpkg": None, "suite": "jessie"}) == "0.1.2-1"
