7133ffc1a011c9a69be978344939885ddede837a
lilydjwg <lilydjwg@gmail.com>
2017-12-22 14:15:05 +0800

    [test] yield_fixture is deprecated

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ async def get_version():
 
     return __call__
 
-@pytest.yield_fixture(scope="module")
+@pytest.fixture(scope="module")
 def event_loop(request):
     """Override pytest-asyncio's event_loop fixture,
        Don't create an instance of the default event loop for each test case.
