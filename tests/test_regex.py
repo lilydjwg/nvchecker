ded2bcd8c52975a49f6f0f96a743415d964ea468
lilydjwg <lilydjwg@gmail.com>
2018-05-08 19:27:45 +0800

    tests: httpbin seems to be overloaded, disabled temporarily

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -4,9 +4,17 @@
 import pytest
 pytestmark = pytest.mark.asyncio
 
-async def test_regex(get_version):
+@pytest.mark.skipif(True,
+                    reason='httpbin is overloaded?')
+async def test_regex_httpbin(get_version):
     assert await get_version("example", {
         "url": "https://httpbin.org/get",
         "regex": '"User-Agent": "(\w+)"',
         "user_agent": "Meow",
     }) == "Meow"
+
+async def test_regex(get_version):
+    assert await get_version("example", {
+        "url": "https://example.net/",
+        "regex": 'for (\w+) examples',
+    }) == "illustrative"
