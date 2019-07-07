981143f62b44cd3c11784bf7f6a95786d52dd8bb
lilydjwg <lilydjwg@gmail.com>
2019-07-07 17:42:11 +0800

    update DEFAULT_USER_AGENT and bump version

--- nvchecker/source/httpclient.py
+++ nvchecker/source/httpclient.py
@@ -1,1 +1,6 @@
-DEFAULT_USER_AGENT = 'lilydjwg/nvchecker'
+# MIT licensed
+# Copyright (c) 2019 lilydjwg <lilydjwg@gmail.com>, et al.
+
+from .. import __version__
+
+DEFAULT_USER_AGENT = 'lilydjwg/nvchecker %s' % __version__
