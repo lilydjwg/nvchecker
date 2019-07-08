1e1f431f9bf301ac2ac3e4f158632da8d0ac0858
lilydjwg <lilydjwg@gmail.com>
2019-07-08 11:40:08 +0800

    fix test_regex

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,14 +1,15 @@
 # MIT licensed
-# Copyright (c) 2013-2017 lilydjwg <lilydjwg@gmail.com>, et al.
+# Copyright (c) 2013-2019 lilydjwg <lilydjwg@gmail.com>, et al.
 
 import pytest
 pytestmark = pytest.mark.asyncio
 
 async def test_regex_httpbin_default_user_agent(get_version):
-    assert await get_version("example", {
+    ua = await get_version("example", {
         "url": "https://httpbin.org/get",
         "regex": r'"User-Agent": "([^"]+)"',
-    }) == "lilydjwg/nvchecker"
+    })
+    assert ua.startswith("lilydjwg/nvchecker")
 
 async def test_regex_httpbin_user_agent(get_version):
     assert await get_version("example", {
