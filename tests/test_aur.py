7e44dd576745e5b0bc6a9fe30d66287e3527d6a6
lilydjwg <lilydjwg@gmail.com>
2019-10-15 18:59:16 +0800

    travis: don't run test_aur

--- tests/test_aur.py
+++ tests/test_aur.py
@@ -1,9 +1,14 @@
 # MIT licensed
 # Copyright (c) 2013-2017 lilydjwg <lilydjwg@gmail.com>, et al.
 
+import os
+
 from flaky import flaky
 import pytest
-pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
+pytestmark = [pytest.mark.asyncio,
+              pytest.mark.needs_net,
+              pytest.mark.skipif(os.environ.get('TRAVIS') == 'true',
+                                 reason="fail too often")]
 
 @flaky(max_runs=10)
 async def test_aur(get_version):
