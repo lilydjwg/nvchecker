58a7ae05ca573ad8602b74e644c56887864f14ae
lilydjwg <lilydjwg@gmail.com>
2020-08-20 17:07:16 +0800

    hackage is flaky

--- tests/test_hackage.py
+++ tests/test_hackage.py
@@ -1,9 +1,11 @@
 # MIT licensed
 # Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
 
+from flaky import flaky
 import pytest
 pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
 
+@flaky(max_runs=10)
 async def test_hackage(get_version):
     assert await get_version("sessions", {
         "source": "hackage",
