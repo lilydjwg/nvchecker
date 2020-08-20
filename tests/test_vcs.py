649799389f97d7f80cb9ba8f3fb3d9742bb7f174
lilydjwg <lilydjwg@gmail.com>
2020-08-20 15:42:50 +0800

    bitbucket no longer supports hg

--- tests/test_vcs.py
+++ tests/test_vcs.py
@@ -14,14 +14,6 @@ async def test_git(get_version):
         "vcs": "git+https://github.com/harry-sanabria/ReleaseTestRepo.git",
     }) == "1.1.2b3cdf6134b07ae6ac77f11b586dc1ae6d1521db"
 
-@pytest.mark.skipif(shutil.which("hg") is None,
-                    reason="requires hg command")
-async def test_mercurial(get_version):
-    assert await get_version("example", {
-        "source": "vcs",
-        "vcs": "hg+https://bitbucket.org/pil0t/testrepo",
-    }) == "1.1.84679e29c7d9"
-
 @pytest.mark.skipif(shutil.which("git") is None,
                     reason="requires git command")
 async def test_git_max_tag(get_version):
