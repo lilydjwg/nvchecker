04a95de7afa4706467bca9ba309bca60ca6c1b98
lilydjwg <lilydjwg@gmail.com>
2020-08-20 15:50:36 +0800

    gitea.com is slow on travis, try harder

--- tests/test_gitea.py
+++ tests/test_gitea.py
@@ -1,10 +1,12 @@
 # MIT licensed
 # Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
 
+from flaky import flaky
 import pytest
 pytestmark = [pytest.mark.asyncio,
               pytest.mark.needs_net]
 
+@flaky(max_runs=10)
 async def test_gitea(get_version):
     ver = await get_version("example", {
         "source": "gitea",
@@ -12,6 +14,7 @@ async def test_gitea(get_version):
     assert len(ver) == 8
     assert ver.isdigit()
 
+@flaky(max_runs=10)
 async def test_gitea_max_tag(get_version):
     assert await get_version("example", {
         "source": "gitea",
@@ -19,6 +22,7 @@ async def test_gitea_max_tag(get_version):
         "use_max_tag": True,
     }) == "v0.4.0"
 
+@flaky(max_runs=10)
 async def test_gitea_max_tag_with_ignored(get_version):
     assert await get_version("example", {
         "source": "gitea",
@@ -27,6 +31,7 @@ async def test_gitea_max_tag_with_ignored(get_version):
         "ignored": "v0.4.0",
     }) == "v0.3.1"
 
+@flaky(max_runs=10)
 async def test_gitea_max_tag_with_include(get_version):
     assert await get_version("example", {
         "source": "gitea",
