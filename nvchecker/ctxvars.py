4f515d75db763d56b9ba3b24c1ad8904d4f60fde
lilydjwg <lilydjwg@gmail.com>
2020-08-21 18:53:09 +0800

    fix type annotations
    [ci skip]

--- nvchecker/ctxvars.py
+++ nvchecker/ctxvars.py
@@ -1,12 +1,15 @@
 # MIT licensed
 # Copyright (c) 2020 lilydjwg <lilydjwg@gmail.com>, et al.
 
+from __future__ import annotations
+
 from contextvars import ContextVar
+from typing import Optional
 
 from . import __version__
 
 DEFAULT_USER_AGENT = 'lilydjwg/nvchecker %s' % __version__
 
 tries = ContextVar('tries', default=1)
-proxy = ContextVar('proxy', default=None)
+proxy: ContextVar[Optional[str]] = ContextVar('proxy', default=None)
 user_agent = ContextVar('user_agent', default=DEFAULT_USER_AGENT)
