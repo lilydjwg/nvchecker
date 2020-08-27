3a6746ed2447bf2d9c3e7d79a5cfe59571582068
lilydjwg <lilydjwg@gmail.com>
2020-08-27 23:23:55 +0800

    update test_android

--- tests/test_android.py
+++ tests/test_android.py
@@ -8,4 +8,4 @@ async def test_android_addon(get_version):
     assert await get_version("android-google-play-apk-expansion", {"android_sdk": "extras;google;market_apk_expansion", "repo": "addon"}) == "1.r03"
 
 async def test_android_package(get_version):
-    assert await get_version("android-sdk-cmake", {"android_sdk": "cmake;", "repo": "package"}) == "3.6.4111459"
+    assert await get_version("android-sdk-cmake", {"android_sdk": "cmake;", "repo": "package"}) == "3.18.1"
