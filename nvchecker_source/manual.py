a2a239bdc6de316c9c905d24d5337da76892fcb0
lilydjwg <lilydjwg@gmail.com>
2020-08-30 17:49:39 +0800

    nvchecker_source/manual: convert to string
    because it's often written as an int or float, especially when
    converting from YAML.

--- nvchecker_source/manual.py
+++ nvchecker_source/manual.py
@@ -2,4 +2,4 @@
 # Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
 
 async def get_version(name, conf, **kwargs):
-  return conf.get('manual').strip() or None
+  return str(conf.get('manual')).strip() or None
