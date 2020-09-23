23bcbf89063e3d1dbf78be78013983751fb44894
Felix Yan <felixonmars@archlinux.org>
2020-09-23 16:51:09 +0800

    npm: Request abbreviated metadata to reduce download size
    Ref:
    https://github.com/npm/registry/blob/master/docs/responses/package-metadata.md

--- nvchecker_source/npm.py
+++ nvchecker_source/npm.py
@@ -5,5 +5,5 @@ NPM_URL = 'https://registry.npmjs.org/%s'
 
 async def get_version(name, conf, *, cache, **kwargs):
   key = conf.get('npm', name)
-  data = await cache.get_json(NPM_URL % key)
+  data = await cache.get_json(NPM_URL % key, headers={"Accept": "application/vnd.npm.install-v1+json"})
   return data['dist-tags']['latest']
