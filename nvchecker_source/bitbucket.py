a8228bb594cef36b1527fc0a51a6328a5561a313
Alexandru Fikl <alexfikl@gmail.com>
2022-02-04 10:31:41 -0600

    bitbucket: use querying and sorting for improved tag search

--- nvchecker_source/bitbucket.py
+++ nvchecker_source/bitbucket.py
@@ -2,12 +2,14 @@
 # Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
 
 from typing import Any, List
+from urllib.parse import urlencode
+
 from nvchecker.api import VersionResult, Entry, AsyncCache
 
 # doc: https://developer.atlassian.com/cloud/bitbucket/rest/api-group-commits/#api-repositories-workspace-repo-slug-commits-get
 BITBUCKET_URL = 'https://bitbucket.org/api/2.0/repositories/%s/commits/%s'
 # doc: https://developer.atlassian.com/cloud/bitbucket/rest/api-group-refs/#api-repositories-workspace-repo-slug-refs-tags-get
-BITBUCKET_MAX_TAG = 'https://bitbucket.org/api/2.0/repositories/%s/refs/tags?sort=-target.date'
+BITBUCKET_MAX_TAG = 'https://bitbucket.org/api/2.0/repositories/%s/refs/tags'
 
 async def get_version(
   name: str, conf: Entry, *,
@@ -17,8 +19,19 @@ async def get_version(
   repo = conf['bitbucket']
   br = conf.get('branch', '')
   use_max_tag = conf.get('use_max_tag', False)
+  use_sorted_tags = conf.get('use_sorted_tags', False)
 
-  if use_max_tag:
+  if use_sorted_tags:
+    url = BITBUCKET_MAX_TAG % repo
+
+    parameters = {'sort': conf.get('sort', '-target.date')}
+    if 'query' in conf:
+        parameters['q'] = conf['query']
+
+    url += '?' + urlencode(parameters)
+    data = await _get_tags(url, max_page=1, cache=cache)
+
+  elif use_max_tag:
     url = BITBUCKET_MAX_TAG % repo
     max_page = conf.get('max_page', 3)
     data = await _get_tags(url, max_page=max_page, cache=cache)
@@ -27,7 +40,7 @@ async def get_version(
     url = BITBUCKET_URL % (repo, br)
     data = await cache.get_json(url)
 
-  if use_max_tag:
+  if use_max_tag or use_sorted_tags:
     version = data
   else:
     version = data['values'][0]['date'].split('T', 1)[0].replace('-', '')
