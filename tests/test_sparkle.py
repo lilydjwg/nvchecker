a9146d4821dacbab351fa80be55c5ea4ac201cb4
Guizai <guizaicn@gmail.com>
2022-02-21 12:19:56 +0000

    fix: sparkle test

--- tests/test_sparkle.py
+++ tests/test_sparkle.py
@@ -8,5 +8,5 @@ pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
 async def test_sparkle(get_version):
     assert await get_version("example", {
         "source": "sparkle",
-        "sparkle": "https://sparkle-project.org/files/sparkletestcast.xml",
-    }) == "2.0"
+        "sparkle": "https://raw.githubusercontent.com/sparkle-project/Sparkle/master/Tests/Resources/testlocalizedreleasenotesappcast.xml",
+    }) == "6.0"
