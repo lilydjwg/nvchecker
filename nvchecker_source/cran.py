e03023bd6cf4c07b4f96eaecdc042b1024265ca0
Pekka Ristola <pekkarr@protonmail.com>
2022-02-22 18:38:49 +0200

    add CRAN support

---
+++ nvchecker_source/cran.py
@@ -0,0 +1,26 @@
+# MIT licensed
+# Copyright (c) 2022 Pekka Ristola <pekkarr [at] protonmail [dot] com>, et al.
+
+from nvchecker.api import session, GetVersionError
+
+CRAN_URL = 'https://cran.r-project.org/package=%s/DESCRIPTION'
+VERSION_FIELD = 'Version: '
+
+async def request(pkg):
+  url = CRAN_URL % pkg
+  res = await session.get(url)
+  return res.body.decode('utf-8', errors='ignore')
+
+async def get_version(name, conf, *, cache, **kwargs):
+  package = conf.get('cran', name)
+
+  desc = await cache.get(package, request)
+
+  for line in desc.splitlines():
+    if line.startswith(VERSION_FIELD):
+      version = line[len(VERSION_FIELD):]
+      break
+  else:
+    raise GetVersionError('Invalid DESCRIPTION file')
+
+  return version
