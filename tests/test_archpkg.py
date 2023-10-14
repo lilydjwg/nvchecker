5dcb3bc36a2edea7c9fed643b1856c53238d4f6f
lilydjwg <lilydjwg@gmail.com>
2023-10-14 15:19:13 +0800

    tests: update

--- tests/test_archpkg.py
+++ tests/test_archpkg.py
@@ -9,7 +9,7 @@ pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
 async def test_archpkg(get_version):
     assert await get_version("base", {
         "source": "archpkg",
-    }) == "3-1"
+    }) == "3-2"
 
 @flaky
 async def test_archpkg_strip_release(get_version):
