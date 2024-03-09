1005bd5f01a59cce9968d8443997a47913b1e7d7
lilydjwg <lilydjwg@gmail.com>
2024-03-09 19:19:06 +0800

    mercurial: update tests

--- tests/test_mercurial.py
+++ tests/test_mercurial.py
@@ -7,5 +7,5 @@ pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
 async def test_mercurial(get_version):
     assert await get_version("example", {
         "source": "mercurial",
-        "mercurial": "https://www.mercurial-scm.org/repo/users/sid0/hg-git",
-    }) == "0.8.0"
+        "mercurial": "https://repo.mercurial-scm.org/hg-website/json-tags",
+    }) == "v1.0"
