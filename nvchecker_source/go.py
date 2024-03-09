4ad3bdb95d745c929e0f24542ab2b25e90e3893a
lilydjwg <lilydjwg@gmail.com>
2024-03-09 18:48:47 +0800

    go: style: unify indentation etc

--- nvchecker_source/go.py
+++ nvchecker_source/go.py
@@ -4,33 +4,33 @@
 from lxml import html
 
 from nvchecker.api import (
-    VersionResult, Entry, AsyncCache, KeyManager,
-    session, GetVersionError, HTTPError
+  VersionResult, Entry, AsyncCache, KeyManager,
+  session, GetVersionError,
 )
 
 GO_PKG_URL = 'https://pkg.go.dev/{pkg}?tab=versions'
 
 
 async def get_version(
-        name: str, conf: Entry, *,
-        cache: AsyncCache, keymanager: KeyManager,
-        **kwargs,
+    name: str, conf: Entry, *,
+    cache: AsyncCache, keymanager: KeyManager,
+    **kwargs,
 ) -> VersionResult:
-    key = tuple(sorted(conf.items()))
-    return await cache.get(key, get_version_impl)
+  key = tuple(sorted(conf.items()))
+  return await cache.get(key, get_version_impl)
 
 
 async def get_version_impl(info) -> VersionResult:
-    conf = dict(info)
-    pkg_name = conf.get('go')
+  conf = dict(info)
+  pkg_name = conf.get('go')
 
-    url = GO_PKG_URL.format(pkg=pkg_name)
-    res = await session.get(url)
-    doc = html.fromstring(res.body.decode())
+  url = GO_PKG_URL.format(pkg=pkg_name)
+  res = await session.get(url)
+  doc = html.fromstring(res.body.decode())
 
-    elements = doc.xpath("//div[@class='Version-tag']/a/text()")
-    try:
-        version = elements[0]
-        return version
-    except IndexError:
-        raise GetVersionError("parse error", pkg_name=pkg_name)
+  elements = doc.xpath("//div[@class='Version-tag']/a/text()")
+  try:
+    version = elements[0]
+    return version
+  except IndexError:
+    raise GetVersionError("parse error", pkg_name=pkg_name)
