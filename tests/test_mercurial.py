4ec12d30fdceb1ce4e2cd0cf74aab7a6e9eb324a
lilydjwg <lilydjwg@gmail.com>
2024-03-09 19:59:01 +0800

    mercurial: tests: update asyncio mark

--- tests/test_mercurial.py
+++ tests/test_mercurial.py
@@ -2,10 +2,13 @@
 # Copyright (c) 2020 Felix Yan <felixonmars@archlinux.org>, et al.
 
 import pytest
-pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
+pytestmark = [
+  pytest.mark.asyncio(scope="session"),
+  pytest.mark.needs_net,
+]
 
 async def test_mercurial(get_version):
-    assert await get_version("example", {
-        "source": "mercurial",
-        "mercurial": "https://repo.mercurial-scm.org/hg-website/json-tags",
-    }) == "v1.0"
+  assert await get_version("example", {
+    "source": "mercurial",
+    "mercurial": "https://repo.mercurial-scm.org/hg-website/json-tags",
+  }) == "v1.0"
