ca011221cf06b86bd743234977a85fed7b477c40
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2025-02-09 14:50:10 -0500

    Use walrus operator and explicitly require python >= 3.8
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- nvchecker_source/pypi.py
+++ nvchecker_source/pypi.py
@@ -20,7 +20,7 @@ async def get_version(name, conf, *, cache, **kwargs):
 
   for version in data['releases'].keys():
     # Skip versions that are marked as yanked.
-    if len(data['releases'][version]) != 0 and data['releases'][version][0]['yanked']:
+    if (vers := data['releases'][version]) and vers[0]['yanked']:
       continue
 
     try:
