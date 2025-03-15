fe60d1a21a9b54306d49a57b6acec067ca5f165d
lilydjwg <lilydjwg@gmail.com>
2025-03-15 11:05:38 +0800

    update tests

--- tests/test_cran.py
+++ tests/test_cran.py
@@ -5,6 +5,7 @@ import pytest
 pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
 
 async def test_cran(get_version):
-    assert await get_version("xml2", {
+    ver = await get_version("xml2", {
         "source": "cran",
-    }) == "1.3.7"
+    })
+    assert ver.startswith("1.3.")
