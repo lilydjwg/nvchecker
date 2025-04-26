c1c9624b23a7f9f0cdae0631e0253897cbda84a7
lilydjwg <lilydjwg@gmail.com>
2025-04-26 09:41:12 +0800

    Revert "tests: skip openvsx because of its outage"
    This reverts commit 4c911c6a8eed00882f020fc7449c7d9237159d5d.
    
    It's back.

--- tests/test_openvsx.py
+++ tests/test_openvsx.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2013-2021 Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>, et al.
 
 import pytest
-pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net, pytest.mark.skip('outage')]
+pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
 
 async def test_openvsx(get_version):
     assert await get_version("usernamehw.indent-one-space", {
