ab4a0f6eb8a982f75093756b2f711b891d1469ea
Lorenzo Pirritano <lorepirri@gmail.com>
2026-05-06 08:51:23 +0200

    format

--- nvchecker_source/git_pygit2.py
+++ nvchecker_source/git_pygit2.py
@@ -10,67 +10,67 @@ from nvchecker.api import RichResult, GetVersionError
 
 
 async def _list_remote_refs_async(key):
-  return await asyncio.to_thread(_list_remote_refs, key)
+    return await asyncio.to_thread(_list_remote_refs, key)
 
 
 def _list_remote_refs(key):
-  git, ref = key
+    git, ref = key
 
-  with tempfile.TemporaryDirectory() as tmpdir:
-    repo = pygit2.init_repository(tmpdir, bare=True)
-    remote = repo.remotes.create_anonymous(git)
-    refs = remote.list_heads()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        repo = pygit2.init_repository(tmpdir, bare=True)
+        remote = repo.remotes.create_anonymous(git)
+        refs = remote.list_heads()
 
-  if ref is None:
-    return refs
-
-  return [r for r in refs if r.name == ref]
-
-
-async def get_version(
-  name, conf, *, cache, keymanager=None
-):
-  git = conf['git']
-
-  use_commit = conf.get('use_commit', False)
-  if use_commit:
-    ref = conf.get('branch')
     if ref is None:
-      ref = 'HEAD'
-      gitref = None
-    else:
-      ref = 'refs/heads/' + ref
-      gitref = ref
+        return refs
 
-    refs = await cache.get((git, ref), _list_remote_refs_async)
-    if not refs:
-      raise GetVersionError('No ref found in upstream repository.', ref=ref)
+    return [r for r in refs if r.name == ref]
 
-    version = str(refs[0].oid)
-    return RichResult(
-      version = version,
-      revision = version,
-      gitref = gitref,
-    )
 
-  refs = await cache.get((git, None), _list_remote_refs_async)
+async def get_version(name, conf, *, cache, keymanager=None):
+    git = conf["git"]
 
-  versions = []
-  for ref in refs:
-    if not ref.name.startswith('refs/tags/'):
-      continue
-    if ref.name.endswith('^{}'):
-      continue
+    use_commit = conf.get("use_commit", False)
+    if use_commit:
+        ref = conf.get("branch")
+        if ref is None:
+            ref = "HEAD"
+            gitref = None
+        else:
+            ref = "refs/heads/" + ref
+            gitref = ref
 
-    version = ref.name.removeprefix('refs/tags/')
-    revision = str(ref.oid)
-    versions.append(RichResult(
-      version = version,
-      revision = revision,
-      gitref = ref.name,
-    ))
+        refs = await cache.get((git, ref), _list_remote_refs_async)
+        if not refs:
+            raise GetVersionError("No ref found in upstream repository.", ref=ref)
 
-  if not versions:
-    raise GetVersionError('No tag found in upstream repository.')
+        version = str(refs[0].oid)
+        return RichResult(
+            version=version,
+            revision=version,
+            gitref=gitref,
+        )
 
-  return versions
+    refs = await cache.get((git, None), _list_remote_refs_async)
+
+    versions = []
+    for ref in refs:
+        if not ref.name.startswith("refs/tags/"):
+            continue
+        if ref.name.endswith("^{}"):
+            continue
+
+        version = ref.name.removeprefix("refs/tags/")
+        revision = str(ref.oid)
+        versions.append(
+            RichResult(
+                version=version,
+                revision=revision,
+                gitref=ref.name,
+            )
+        )
+
+    if not versions:
+        raise GetVersionError("No tag found in upstream repository.")
+
+    return versions
--- tests/test_git_pygit2.py
+++ tests/test_git_pygit2.py
@@ -6,37 +6,54 @@ import pytest
 # Skip all tests in this file if pygit2 is not installed
 pygit2_available = True
 try:
-  import pygit2
+    import pygit2
 except ImportError:
-  pygit2_available = False
+    pygit2_available = False
 
 pytestmark = [
-  pytest.mark.asyncio,
-  pytest.mark.needs_net,
-  pytest.mark.skipif(not pygit2_available, reason="needs pygit2"),
+    pytest.mark.asyncio,
+    pytest.mark.needs_net,
+    pytest.mark.skipif(not pygit2_available, reason="needs pygit2"),
 ]
 
 
 async def test_git_pygit2(get_version):
-    assert await get_version("example", {
-        "source": "git_pygit2",
-        "git": "https://gitlab.com/gitlab-org/gitlab-test.git",
-    }) == "v1.1.1"
+    assert (
+        await get_version(
+            "example",
+            {
+                "source": "git_pygit2",
+                "git": "https://gitlab.com/gitlab-org/gitlab-test.git",
+            },
+        )
+        == "v1.1.1"
+    )
 
 
 async def test_git_pygit2_commit(get_version):
-    assert await get_version("example", {
-        "source": "git_pygit2",
-        "git": "https://gitlab.com/gitlab-org/gitlab-test.git",
-        "use_commit": True,
-    }) == "ddd0f15ae83993f5cb66a927a28673882e99100b"
+    assert (
+        await get_version(
+            "example",
+            {
+                "source": "git_pygit2",
+                "git": "https://gitlab.com/gitlab-org/gitlab-test.git",
+                "use_commit": True,
+            },
+        )
+        == "ddd0f15ae83993f5cb66a927a28673882e99100b"
+    )
 
 
 async def test_git_pygit2_commit_branch(get_version):
-    assert await get_version("example", {
-        "source": "git_pygit2",
-        "git": "https://gitlab.com/gitlab-org/gitlab-test.git",
-        "use_commit": True,
-        "branch": "with-executables",
-    }) == "6b8dc4a827797aa025ff6b8f425e583858a10d4f"
-
+    assert (
+        await get_version(
+            "example",
+            {
+                "source": "git_pygit2",
+                "git": "https://gitlab.com/gitlab-org/gitlab-test.git",
+                "use_commit": True,
+                "branch": "with-executables",
+            },
+        )
+        == "6b8dc4a827797aa025ff6b8f425e583858a10d4f"
+    )
