b07d8b5bd1febb49c61dd9b88376dd96f355a6f7
lilydjwg <lilydjwg@gmail.com>
2026-08-04 13:08:55 +0800

    update test_gitea
    closes #345.

--- tests/test_gitea.py
+++ tests/test_gitea.py
@@ -19,8 +19,8 @@ async def test_gitea_max_tag_with_include(get_version):
         "source": "gitea",
         "gitea": "gitea/tea",
         "use_max_tag": True,
-        "include_regex": r'v0\.9.*',
-    }) == "v0.9.2"
+        "include_regex": r'v0\.14.*',
+    }) == "v0.14.2"
 
 async def test_gitea_latest_release(get_version):
     ver = await get_version("example", {
