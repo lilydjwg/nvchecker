68b6f3175a803928f19ca50f49b10e3d546e496b
lilydjwg <lilydjwg@gmail.com>
2026-09-17 11:33:49 +0800

    tests: skip test_openvsx for GitHub Actions (503 error)

--- tests/test_openvsx.py
+++ tests/test_openvsx.py
@@ -1,8 +1,14 @@
 # MIT licensed
 # Copyright (c) 2013-2021 Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>, et al.
 
+import os
 import pytest
-pytestmark = [pytest.mark.asyncio, pytest.mark.needs_net]
+
+pytestmark = [
+    pytest.mark.asyncio,
+    pytest.mark.needs_net,
+    pytest.mark.skipif(bool(os.environ.get('GITHUB_RUN_ID')), reason="503 for GitHub Actions"),
+]
 
 async def test_openvsx(get_version):
     assert await get_version("usernamehw.indent-one-space", {
